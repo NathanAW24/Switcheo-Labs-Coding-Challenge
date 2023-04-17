6cfc442c618b60d4ac6f3a1c08656cc6e42fe390
Nathan Aldrich Wiryawan <nathan_wiryawan@mymail.sutd.edu.sg>
2023-04-17 16:46:35 +0800

    form functionalities cleared, left with designing

--- src/problem2-next/src/components/TokenForm.tsx
+++ src/problem2-next/src/components/TokenForm.tsx
@@ -1,79 +1,99 @@
-import React , { FormEvent, useState } from 'react';
+import React, { FormEvent, useState } from 'react'
+import FormResponse from './FormResponse'
 
 type FormData = {
-    ethAddress: string;
-    amountToSend: number;
-    otpAuth: string;
-  };
-  
-const TokenForm:any = () => {
+  ethAddress: string
+  amountToSend: number
+  otpAuth: string
+}
+
+const TokenForm: any = () => {
   const [formData, setFormData] = useState<FormData>({
     ethAddress: '',
     amountToSend: 0,
     otpAuth: '',
-  });
+  })
+
+  const [showEmptyFormWarning, setShowEmptyFormWarning] = useState<boolean>(false)
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-  };
+    const { name, value } = e.target
+    setFormData({ ...formData, [name]: value })
+  }
 
   const handleSubmit = (e: FormEvent) => {
-    e.preventDefault();
-    console.log('Form data:', formData);
+    e.preventDefault()
+    const { ethAddress, amountToSend, otpAuth } = formData
+    console.log('Form data:', formData)
+
+    // empty form handling, so that the empty data is not passed to database
+    if (!ethAddress || !amountToSend || !otpAuth) {
+      setShowEmptyFormWarning(true)
+      setTimeout(() => {
+        setShowEmptyFormWarning(false)
+      }, 3000)
+      return
+    }
 
     // Send the data to the server, e.g., using fetch or axios
-  };
+
+    // after submit we want the form to be empty
+    setFormData({
+      ethAddress: '',
+      amountToSend: 0,
+      otpAuth: '',
+    })
+  }
 
   return (
     <form onSubmit={handleSubmit} className="space-y-4">
-      <div>
-        <label htmlFor="ethAddress" className="block text-sm font-medium">
-          ETH Address
-        </label>
-        <input
-          type="text"
-          id="ethAddress"
-          name="ethAddress"
-          value={formData.ethAddress}
-          onChange={handleChange}
-          className="w-full px-3 py-2 border border-gray-300 rounded-md"
-        />
+      <div className="">
+        <div>
+          <label htmlFor="ethAddress" className="block text-sm font-medium">
+            ETH Address
+          </label>
+          <input
+            type="text"
+            id="ethAddress"
+            name="ethAddress"
+            value={formData.ethAddress}
+            onChange={handleChange}
+            className="text-black"
+          />
+        </div>
+        <div>
+          <label htmlFor="amountToSend" className="block text-sm font-medium">
+            Amount to send
+          </label>
+          <input
+            type="number"
+            id="amountToSend"
+            name="amountToSend"
+            value={formData.amountToSend}
+            onChange={handleChange}
+            className="text-black"
+          />
+        </div>
+        <div>
+          <label htmlFor="otpAuth" className="block text-sm font-medium">
+            OTP Authentication
+          </label>
+          <input
+            type="text"
+            id="otpAuth"
+            name="otpAuth"
+            value={formData.otpAuth}
+            onChange={handleChange}
+            className="text-black"
+          />
+        </div>
+        <FormResponse isEmpty={showEmptyFormWarning} />
+        <button type="submit" className="bg-blue-800">
+          Send Tokens
+        </button>
       </div>
-      <div>
-        <label htmlFor="amountToSend" className="block text-sm font-medium">
-          Amount to send
-        </label>
-        <input
-          type="number"
-          id="amountToSend"
-          name="amountToSend"
-          value={formData.amountToSend}
-          onChange={handleChange}
-          className="w-full px-3 py-2 border border-gray-300 rounded-md"
-        />
-      </div>
-      <div>
-        <label htmlFor="otpAuth" className="block text-sm font-medium">
-          OTP Authentication
-        </label>
-        <input
-          type="text"
-          id="otpAuth"
-          name="otpAuth"
-          value={formData.otpAuth}
-          onChange={handleChange}
-          className="w-full px-3 py-2 border border-gray-300 rounded-md"
-        />
-      </div>
-      <button
-        type="submit"
-        className="w-full px-4 py-2 text-white bg-blue-600 rounded-md hover:bg-blue-700"
-      >
-        Send Tokens
-      </button>
     </form>
-  );
-};
+  )
+}
 
-export default TokenForm;
+export default TokenForm
--- src/problem2-next/src/pages/index.tsx
+++ src/problem2-next/src/pages/index.tsx
@@ -6,8 +6,8 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex items-center justify-center w-screen h-screen">
       <TokenForm />
     </div>
-    )
+  )
 }
