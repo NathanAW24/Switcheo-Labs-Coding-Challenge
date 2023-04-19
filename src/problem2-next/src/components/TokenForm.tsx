d224f9fa468bd5e0247c1b1c6321dffa603bd4e2
Nathan Aldrich Wiryawan <nathan_wiryawan@mymail.sutd.edu.sg>
2023-04-19 18:49:56 +0800

    design visually appealing component in the form

--- src/problem2-next/src/components/TokenForm.tsx
+++ src/problem2-next/src/components/TokenForm.tsx
@@ -1,5 +1,4 @@
 import React, { FormEvent, useState } from 'react'
-import FormResponse from './FormResponse'
 
 type FormData = {
   ethAddress: string
@@ -27,7 +26,7 @@ const TokenForm: any = () => {
     console.log('Form data:', formData)
 
     // empty form handling, so that the empty data is not passed to database
-    if (!ethAddress || !amountToSend || !otpAuth) {
+    if (!ethAddress || amountToSend === 0 || !otpAuth) {
       setShowEmptyFormWarning(true)
       setTimeout(() => {
         setShowEmptyFormWarning(false)
@@ -36,6 +35,7 @@ const TokenForm: any = () => {
     }
 
     // Send the data to the server, e.g., using fetch or axios
+    console.log('data sent!', formData)
 
     // after submit we want the form to be empty
     setFormData({
@@ -46,53 +46,71 @@ const TokenForm: any = () => {
   }
 
   return (
-    <form onSubmit={handleSubmit} className="space-y-4">
-      <div className="">
-        <div>
-          <label htmlFor="ethAddress" className="block text-sm font-medium">
+    <div className="w-full max-w-xs">
+      <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmit}>
+        <h1 className="text-xl text-center text-gray-700 font-bold my-3">Send Your ETH Coins!</h1>
+        {showEmptyFormWarning && (
+          <div className="bg-red-200 text-red-900 px-4 py-2 mb-2 rounded">
+            Please fill out all fields before submitting the form!
+          </div>
+        )}
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="ethAddress">
             ETH Address
           </label>
           <input
-            type="text"
+            className="shadow bg-gray-50 appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             id="ethAddress"
             name="ethAddress"
+            type="text"
+            placeholder="ETH Address"
             value={formData.ethAddress}
             onChange={handleChange}
-            className="text-black"
           />
         </div>
-        <div>
-          <label htmlFor="amountToSend" className="block text-sm font-medium">
-            Amount to send
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="amountToSend">
+            Amount to Send
           </label>
           <input
-            type="number"
+            className="shadow bg-gray-50 appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             id="amountToSend"
             name="amountToSend"
+            type="number"
+            placeholder="0"
+            min="0"
             value={formData.amountToSend}
             onChange={handleChange}
-            className="text-black"
           />
         </div>
-        <div>
-          <label htmlFor="otpAuth" className="block text-sm font-medium">
-            OTP Authentication
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="otpAuth">
+            One Time Password
           </label>
           <input
-            type="text"
+            className="shadow bg-gray-50 appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             id="otpAuth"
             name="otpAuth"
+            type="text"
+            placeholder="OTP"
+            min="0"
             value={formData.otpAuth}
             onChange={handleChange}
-            className="text-black"
           />
         </div>
-        <FormResponse isEmpty={showEmptyFormWarning} />
-        <button type="submit" className="bg-blue-800">
-          Send Tokens
-        </button>
-      </div>
-    </form>
+        <div className="flex items-center justify-center mt-3">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
+            Submit
+          </button>
+        </div>
+        <p className="text-center text-gray-500 text-xs pt-3">
+          &copy;2022 Switcheo Labs Pte. Ltd. All rights reserved.
+        </p>
+      </form>
+    </div>
   )
 }
 
--- src/problem2-next/src/pages/index.tsx
+++ src/problem2-next/src/pages/index.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center w-screen h-screen">
+    <div className="flex justify-center items-center h-screen bg-gray-100">
       <TokenForm />
     </div>
   )
